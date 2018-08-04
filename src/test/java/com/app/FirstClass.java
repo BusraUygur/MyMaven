178dce896c9db110f20718c321f07ce833b6b4fe
busrauygur <bnuygur@gmail.com>
2018-08-04 17:18:15 -0400

    2

--- src/test/java/com/app/FirstClass.java
+++ src/test/java/com/app/FirstClass.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstClass {
-
+    
 	@Test
 	public void test() {
 		WebDriverManager.chromedriver().setup();
